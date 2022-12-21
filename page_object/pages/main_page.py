0fbf1f1e49b8b4ae85f9a83fc52ce5a07f8385ad
Terekhinia <46753867+Terekhinia@users.noreply.github.com>
2022-12-21 15:13:16 +0300

    Delete page_object directory

--- page_object/pages/main_page.py
+++
@@ -1,9 +0,0 @@
-from .base_page import BasePage
-from .locators import MainPageLocators
-
-class MainPage(BasePage):
-
-    def select_section_allproducts(self):
-        section_allproducts = self.browser.find_element(*MainPageLocators.SECTION_ALLPRODUCTS)
-        section_allproducts.click()
-
